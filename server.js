77a73eefb37ee7c70c20d842aab4233f7c7a9c8c
rayGreycloud <rayGreycloud@gmail.com>
2016-10-04 15:38:44 -0700

    Add bodyParser and post data to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -18,18 +19,21 @@ var UserSchema = new mongoose.Schema({
 
 var User = mongoose.model('User', UserSchema);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res, next) {
   res.json("Home Page!!");
 });
 
-app.get('/create-user', function(req, res, next) {
+app.post('/create-user', function(req, res, next) {
   var user = new User();
-  user.name = "Darth";
-  user.age = 81;
+  user.name = req.body.name;
+  user.age = req.body.age;
   user.save(function(err) {
-    if (err) next(err);
+    if (err) console.log(err);
     res.json(user);
-  })
+  });
 });
 
 app.listen(3000, function(err) {
