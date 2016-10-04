31e02caee666832e2345afa7c0859202770a95df
rayGreycloud <rayGreycloud@gmail.com>
2016-10-04 14:00:26 -0700

    Demo of connecting to mLab db

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+mongoose.connect('mongodb://root:abc123@ds049486.mlab.com:49486/mongoose-arash', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database connected");
+  }
+});
+
+var UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+});
+
+var User = mongoose.model('User', UserSchema);
+
 app.get('/', function(req, res, next) {
   res.json("Home Page!!");
 });
 
-app.get('/:name', function(req, res, next) {
-    res.json("Hello " + req.params.name);
+app.get('/create-user', function(req, res, next) {
+  var user = new User();
+  user.name = "Darth";
+  user.age = 81;
+  user.save(function(err) {
+    if (err) next(err);
+    res.json(user);
+  })
 });
 
 app.listen(3000, function(err) {
